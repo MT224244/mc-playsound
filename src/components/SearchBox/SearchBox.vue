b25e72701bf904aa0e486d09c96bf86f986f647a
MT224244 <mt224244@gmail.com>
2021-02-17 11:49:14 +0900

    Add the SearchBox component

---
+++ src/components/SearchBox/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-input
+        dense
+        filled
+        square
+        clearable
+        label="Search"
+        v-model="searchText"
+        @input="qInput_onInput"
+    >
+        <template #prepend>
+            <q-icon name="mdi-magnify"/>
+        </template>
+    </q-input>
+</template>
+
+<script lang="ts">
+import { Component, PropSync, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SearchBox extends Vue {
+    @PropSync('value')
+    public searchText!: string;
+
+    public qInput_onInput(value: string) {
+        this.$emit('input', value);
+    }
+}
+</script>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="q-ma-md">
-        <SelectVersion v-model="selectedVersion"/>
+    <div class="column no-wrap">
+        <div class="row q-mt-md q-mx-md">
+            <div class="col-4">
+                <SelectVersion
+                    class="q-mr-sm"
+                    v-model="selectedVersion"
+                    @input="selectVersion_onInput"
+                />
+            </div>
+            <div class="col-8">
+                <SearchBox
+                    class="q-ml-sm"
+                    v-model="searchText"
+                    @input="searchBox_onInput"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,13 +23,26 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 import { SelectVersion } from '@/components/SelectVersion';
+import { SearchBox } from '@/components/SearchBox';
 
 @Component({
     components: {
-        SelectVersion
+        SelectVersion,
+        SearchBox
     }
 })
 export default class Home extends Vue {
     public selectedVersion = '';
+    public searchText = '';
+
+    public selectVersion_onInput() {
+        // TODO: Write the processing when value changed.
+        console.log(this.selectedVersion);
+    }
+
+    public searchBox_onInput() {
+        // TODO: Write the processing when value changed.
+        console.log(this.searchText);
+    }
 }
 </script>
