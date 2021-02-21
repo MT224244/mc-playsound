1485045c7bbc0d5cfcda76125678b826cdb542cb
MT224244 <mt224244@gmail.com>
2021-02-21 13:19:50 +0900

    Change build settings

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,9 @@ module.exports = {
                         'zip',
                         'nsis'
                     ]
+                },
+                nsis: {
+                    oneClick: false
                 }
             }
         }
